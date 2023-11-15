c2159cd5393da241f4f133d8b39003dead72fa9e
Ariana Espinoza <espinoza.salinas.ariana@gmail.com>
2023-11-15 00:07:34 -0800

    Version 1.0

--- frontend/src/components/protectedRoutes.js
+++
@@ -1,7 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom"
-export const ProtectedRoutes = ({isLogged}) =>{
-    if(!isLogged){
-        return <Navigate to="/"/>
-    }
-    return <Outlet/>
-}
